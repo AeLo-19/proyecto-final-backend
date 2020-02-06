0f7c4a026ed2d557233b0aee6634719d4ecc0e8c
AeLo-19 <adrianeduardo.lopezo@gmail.com>
2020-02-06 23:28:46 +0000

    primer modelo de clases

--- src/models.py
+++ src/models.py
@@ -2,6 +2,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    __abstract__=True
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    age = db.Column(db.Date, unique=False, nullable=False)
+    phone = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return  '<User %r>' % self.username
+    
+    def __hash__(self):
+        return hash((self.password, self.phone))
+    
+    
+
+class Paciente(db.User):
+    id = db.Column(db.Integer, primary_key=True)
+    cedula = db.Column(db.Integer, unique=True, nullable=False)
+
+class Doctor(db.User):
+    id = db.Column(db.Integer, primary_key=True)
+    certificado = db.Column(db.String(250), unique=False, nullable=False)
+
+class Cita(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, unique=False, nullable=False)
+    state = db.Column(db.Boolean)
+    price_tot = db.Column(db.Integer, unique=False, nullable=False)
+
+class Tratamiento(db.Column):
+    id = db.Column(db.Integer, primary_key=True)
+    tratamiento_name = db.Column(db.String(125), unique=True, nullable=False)
+    descripcion = db.Column(db.String(250), unique=True, nullable=True)
+    price = db.Column(db.int, unique=False, nullable=False)
+
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
